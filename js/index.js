123e5e88d8976674d3f9aaf6dcae0a51bf2b893b
boboil <boboilodarchenko@gmail.com>
2022-05-21 13:25:46 +0300

    added fixed description  block

--- js/index.js
+++ js/index.js
@@ -11,56 +11,70 @@ function parseJsaon() {
             initMap()
         });
 }
+
 function prepareTemplate(array) {
     points = [];
     array.forEach(point => {
         let workingTime = point.open247 ? '24/7' : 'не кргулосуточно';
-        let description = `<div class="filter-top">
-                    <div class="info-top">
-                        <div class="hug-left">
-                                <span class="green-hug">
-                                    ${point.kvt} КВт
-                                </span>
-                        </div>
-                        <div class="hug-right">
+        let description = `<div class="object" id="object">
+    <div class="object-controls">
+        <button class="object-edit">
+            <svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M0 15.4601V18.5001C0 18.7801 0.22 19.0001 0.5 19.0001H3.54C3.67 19.0001 3.8 18.9501 3.89 18.8501L14.81 7.94006L11.06 4.19006L0.15 15.1001C0.0500001 15.2001 0 15.3201 0 15.4601ZM17.71 5.04006C18.1 4.65006 18.1 4.02006 17.71 3.63006L15.37 1.29006C14.98 0.900059 14.35 0.900059 13.96 1.29006L12.13 3.12006L15.88 6.87006L17.71 5.04006Z" fill="currentColor"/>
+            </svg>
 
-                        </div>
-                    </div>
-                    <div class="address-top">
-                        <span>${point.full_name}</span>
-                    </div>
-                    <div class="working-block">
-                        <h2>${point.name}</h2>
-                        <div class="working-time">
-                                <span>${point.working_time}</span>
-                        </div>
-                    </div>
-                    <div class="working-address">
-                        <span>${point.address}</span>
-                        <span>${point.phone}</span>
-                    </div>
-                    <div class="road-link">
-                        <a href="${point.url}">Проложить маршрут</a>
-                    </div>
-                    <!--<div class="cost-block">-->
-                        <!--<p>Стоимость 2500₽/Квт</p>-->
-                        <!--<p>Стоимость 2500₽/Квт</p>-->
-                        <!--<p>Стоимость 2500₽/Квт</p>-->
-                    <!--</div>-->
-                    <div class="active-block">
-                        <span>${point.description}</span>
-                    </div>
-                    <div class="station-info">
-                        <img src="./images/station-icon.png" alt="station-icon" class="station-img">
-                        <div class="id-info"><span>Three Phase (EU) (currently same ID)</span>
-                            <div class="count-info">Станций: ${point.quantity} <span> ${point.kvt}Квт</span></div>
-                        </div>
-                    </div>
-                </div>`;
+        </button>
+        <button class="object-share">
+            <svg width="22" height="18" viewBox="0 0 22 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M18.66 0.990044C16.02 -0.809956 12.76 0.0300438 11 2.09004C9.24 0.0300438 5.98 -0.819956 3.34 0.990044C1.94 1.95004 1.06 3.57004 1 5.28004C0.86 9.16004 4.3 12.27 9.55 17.04L9.65 17.13C10.41 17.82 11.58 17.82 12.34 17.12L12.45 17.02C17.7 12.26 21.13 9.15004 21 5.27004C20.94 3.57004 20.06 1.95004 18.66 0.990044ZM11.1 15.55L11 15.65L10.9 15.55C6.14 11.24 3 8.39004 3 5.50004C3 3.50004 4.5 2.00004 6.5 2.00004C8.04 2.00004 9.54 2.99004 10.07 4.36004H11.94C12.46 2.99004 13.96 2.00004 15.5 2.00004C17.5 2.00004 19 3.50004 19 5.50004C19 8.39004 15.86 11.24 11.1 15.55Z" fill="currentColor"/>
+            </svg>
+        </button>
+    </div>
+    <div class="object-power green">
+        ${point.kvt} КВт
+    </div>
+    <div class="object-plugs">
+        ${point.full_name}
+    </div>
+    <div class="object-title">
+        ${point.name}
+    </div>
+    <div class="object-worktime">
+        ${point.working_time}
+    </div>
+    <address class="object-address">
+        ${point.address}
+    </address>
+    <a href="tel:${point.phone}" class="object-phone">
+        ${point.phone}
+    </a>
+    <a href="https://maps.google.com/maps?daddr=50.94135,34.77725" target="_blank" class="object-roadmap">
+        Проложить маршрут
+    </a>
+    <div class="object-availability">
+        <div class="availability-item red">
+            <div class="plug">
+                <img src="images/station-icon.png" alt="" />
+            </div>
+            <div class="text">
+                <div class="title">
+                    Three Phase (EU) (currently same ID)
+                </div>
+                <div class="amount">
+                    Станций: ${point.quantity}
+                </div>
+                <div class="power">
+                    ${point.kvt}Квт
+                </div>
+            </div>
+        </div>
+    </div>
+</div>`;
         points.push([{lat: parseFloat(point.latitude), lng: parseFloat(point.longitude)}, description])
     })
 
 }
+
 function filterPoints(elem) {
 
     let paid = false, unpaid = false, fast = false, phase = false;
@@ -74,7 +88,6 @@ function filterPoints(elem) {
         phase = true;
 
 
-    // parseJsaon();
 
     axios.get('https://demonstration.org.ua/laravel/public/points-get').then((response) => {
         pointsHandler = response.data;
@@ -94,7 +107,6 @@ function filterPoints(elem) {
         initMap()
     });
 }
-
 
 
 let getJSON = function (url, callback) {
@@ -152,14 +164,16 @@ function initMap() {
             icon: image,
             title: `${title}`,
             label: ``,
-            optimized: false,
+            optimized: true,
         });
 
         // Add a click listener for each marker, and set up the info window.
         marker.addListener("click", () => {
-            infoWindow.close();
-            infoWindow.setContent(marker.getTitle());
-            infoWindow.open(marker.getMap(), marker);
+            let describe = marker.title,
+                block = document.getElementById('object');
+            block.style.display = "block";
+            block.innerHTML = describe
+
         });
     });
 }
